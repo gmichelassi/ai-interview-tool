d32bed275f386abda902e6638394ead1496fb062
Gabriel Michelassi <gabrielmichelassi10@hotmail.com>
2025-03-11 11:15:05 -0300

    feat: added application entrypoint

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_apscheduler import APScheduler
+from flask_cors import CORS
+
+from api import blueprint
+from api.errors import internal_error_handler, not_found_error_handler
+from application import ApplicationConfig
+from scheduler import register_jobs
+
+load_dotenv()
+
+server = Flask(__name__)
+server.config.from_object(ApplicationConfig())
+server.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+flask_scheduler = APScheduler()
+flask_scheduler.init_app(server)
+flask_scheduler.start()
+
+server.register_blueprint(blueprint, url_prefix='/api')
+server.register_error_handler(404, not_found_error_handler)
+server.register_error_handler(500, internal_error_handler)
+
+register_jobs(flask_scheduler)
+
+CORS(server)
+
+
+if __name__ == '__main__':
+    server.run(debug=False, host="0.0.0.0", port=int(os.getenv("PORT", default="5001")))
